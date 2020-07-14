e571e2cab50cf872a2a3a3c548e296ccb034ba8e
Shawn Pang <shawnpangcanada@gmail.com>
2020-07-14 13:50:59 -0400

    rewrote the search module with Scale SERP api for google search

--- back_end/search.py
+++ back_end/search.py
@@ -1,7 +1,34 @@
-from googlesearch import search
+import requests
+
+__params_template = {
+  'api_key': '5C42256FAF1E41E99E269259A6F52DDD',
+  'q': ''
+}
 
 
-query = "Daily Kos"
+def findNews(source, title):
+  # set up the request parameters
+  params = __params_template
+  params['q'] = "site:{0} {1}".format(source, title)
+  print("Param_tem = " + str(__params_template))
+  print("Param = " + str(params))
 
-for j in search(query, tld="com", num=1, stop=10, pause=2):
-    print(j)
+
+  # make the http GET request to Scale SERP
+  api_result = requests.get('https://api.scaleserp.com/search', params)
+
+
+  # package the needed data and return the results
+
+  organic_results = api_result.json()["organic_results"]
+  newsResult = {
+    "title":organic_results[0]["title"],
+    "link":organic_results[0]["link"],
+    "snippet":organic_results[0]["snippet"]
+  }
+
+  return newsResult
+
+
+if __name__ == '__main__':
+    print(findNews("wsj.com","Trump Visa"))
